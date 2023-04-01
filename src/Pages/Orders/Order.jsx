6daa49abbe2d26f221e664b655b6c3904cdcc87d
Haikal Muhammad z g <haikalmuhammadzg@Haikals-MacBook-Pro.local>
2023-04-01 13:44:18 +0700

    haikal

--- src/Pages/Orders/Order.jsx
+++ src/Pages/Orders/Order.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import "./Order.scss";
+import OrderTable from "../../Components/Order-Table/OrderTable";
 
 const Order = () => {
   return (
-    <div>Order</div>
-  )
-}
+    <>
+      <div className="Order_wrapper">
+        <div className="banner Order_wrapper_banner">
+          <h1>Your Orders</h1>
+        </div>
+      </div>
+      <div className="Order_wrapper_content">
+        <div className="content">
+          <h2 className="Order_wrapper_content_text">All Orders</h2>
+          <OrderTable />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Order
+export default Order;
