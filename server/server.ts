401893bd8f079827755296dff121b8c57d86ba33
Almas Mirzandi <almasmirzandir@gmail.com>
2023-04-06 21:17:45 +0700

    run docker compose

--- server/server.ts
+++ server/server.ts
@@ -2,10 +2,16 @@ import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+dotenv.config();
 
-const port = 8000;
+const port = process.env.APP_PORT;
 
 const app: Express = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from express");
