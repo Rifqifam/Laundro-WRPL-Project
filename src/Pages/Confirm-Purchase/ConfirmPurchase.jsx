25ed260082f4e56651d1ca5bb0ae0a63a5935ad1
rifqifam <muhammad.rifqi.fameizy@mail.ugm.ac.id>
2023-04-17 01:04:24 +0700

    finished append data on form submit

--- src/Pages/Confirm-Purchase/ConfirmPurchase.jsx
+++ src/Pages/Confirm-Purchase/ConfirmPurchase.jsx
@@ -1,64 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./ConfirmPurchase.scss";
 
+import data from "../../Database/placeholder.json";
+
 const ConfirmPurchase = () => {
-  return (
-    <div class="confirmpage_wrapper">
-      <div class="confirmpage_wrapper_input_input_wrapper">
-        <div className="confirmpage_wrapper_title_wrapper">
-          <h1 className="confirmpage_wrapper_title_wrapper_title">
-            Confirm Your Order
-          </h1>
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_box">
-          <label className="confirmpage_wrapper_input_name">Full Name</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_box">
-          <label className="confirmpage_wrapper_input_name">Email</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_box">
-          <label className="confirmpage_wrapper_input_name">Phone Number</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_box">
-          <label className="confirmpage_wrapper_input_name">Voucher</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_box">
-          <label className="confirmpage_wrapper_input_name">Laundry</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_box">
-          <label className="confirmpage_wrapper_input_name">Package</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_address_box">
-          <label className="confirmpage_wrapper_input_name">Address</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div className="confirmpage_wrapper_input_input_wrapper_box">
-          <label className="confirmpage_wrapper_input_name">City</label>
-          <input type="text" placeholder="Type here" />
-        </div>
-        <div>
-          <label className="confirmpage_wrapper_input_name">Total Price</label>
-          <p className="confirmpage_wrapper_input_input_wrapper_price">
-            Rp.20.000
-          </p>
-        </div>
-        <div className="confirmpage_wrapper_button_wrapper">
-          <Link to="/payment">
-            <button className="confirmpage_wrapper_button_wrapper_button">
-              Order Now
-            </button>
-          </Link>
-        </div>
+   function getTodayDate() {
+      const today = new Date();
+      const dd = String(today.getDate()).padStart(2, "0");
+      const mm = String(today.getMonth() + 1).padStart(2, "0"); // January is 0!
+      const yyyy = today.getFullYear();
+
+      return dd + "-" + mm + "-" + yyyy;
+   }
+   const [formData, setFormData] = useState({
+      name: "",
+      date: getTodayDate(),
+      price: 20000,
+   });
+
+   const handleFormSubmit = (event) => {
+      event.preventDefault();
+
+      // Create a new object with the form data
+      const newOrder = {
+         id: data.length + 1,
+         name: formData.name,
+         date: formData.date,
+         price: formData.price,
+         status: "Order Placed",
+      };
+
+      // Update the local JSON file with the new order
+      data.push(newOrder);
+
+      // Reset the form data
+      setFormData({ name: "", date: "", price: 0 });
+
+      console.log(data);
+   };
+
+   const handleInputChange = (event) => {
+      const { name, value } = event.target;
+      setFormData({ ...formData, [name]: value });
+   };
+   return (
+      <div class='confirmpage_wrapper'>
+         <div class='confirmpage_wrapper_input_input_wrapper'>
+            <form action='' onSubmit={handleFormSubmit}>
+               <div className='confirmpage_wrapper_title_wrapper'>
+                  <h1 className='confirmpage_wrapper_title_wrapper_title'>
+                     Confirm Your Order
+                  </h1>
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_box'>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Full Name
+                  </label>
+                  <input type='text' placeholder='Type here' />
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_box'>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Email
+                  </label>
+                  <input type='text' placeholder='Type here' />
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_box'>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Phone Number
+                  </label>
+                  <input type='text' placeholder='Type here' />
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_box'>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Voucher
+                  </label>
+                  <input type='text' placeholder='Type here' />
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_box'>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Laundry
+                  </label>
+                  <input
+                     type='text'
+                     placeholder='Type here'
+                     name='name'
+                     value={formData.name}
+                     onChange={handleInputChange}
+                  />
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_box'>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Package
+                  </label>
+                  <input type='text' placeholder='Type here' />
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_address_box'>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Address
+                  </label>
+                  <input type='text' placeholder='Type here' />
+               </div>
+               <div className='confirmpage_wrapper_input_input_wrapper_box'>
+                  <label className='confirmpage_wrapper_input_name'>City</label>
+                  <input type='text' placeholder='Type here' />
+               </div>
+               <div>
+                  <label className='confirmpage_wrapper_input_name'>
+                     Total Price
+                  </label>
+                  <p className='confirmpage_wrapper_input_input_wrapper_price'>
+                     Rp.20.000
+                  </p>
+               </div>
+               <div className='confirmpage_wrapper_button_wrapper'>
+                  <button
+                     className='confirmpage_wrapper_button_wrapper_button'
+                     type='submit'
+                  >
+                     <Link to='/payment'>Order Now</Link>
+                  </button>
+               </div>
+            </form>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default ConfirmPurchase;
--- src/Pages/Payment-Page/Payment.jsx
+++ src/Pages/Payment-Page/Payment.jsx
@@ -1,53 +1,59 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Payment.scss";
 
 const Payment = () => {
-  return (
-    <div className="container">
-      <div className="card">
-        <div className="rightside">
-          <form action="">
-            <h1>Checkout</h1>
-            <h2>Payment Information</h2>
-            <p>Bank Name</p>
-            <select
-              className="inputbox"
-              name="bank_name"
-              id="bank_name"
-              required
-            >
-              <option value="">--Select Bank--</option>
-              <option value="Bank_Papua">Bank Papua</option>
-              <option value="BNI">BNI</option>
-              <option value="Mandiri">Mandiri</option>
-              <option value="BCA">BCA</option>
-              <option value="BSI">BSI</option>
-              <option value="BRI">BRI</option>
-            </select>
-            <p>Cardholder Name</p>
-            <input type="text" className="inputbox" name="name" required />
-            <p>Card Number</p>
-            <input
-              type="number"
-              className="inputbox"
-              name="card_number"
-              id="card_number"
-              required
+   return (
+      <div className='container'>
+         <div className='card'>
+            <div className='rightside'>
+               <form action=''>
+                  <h1>Checkout</h1>
+                  <h2>Payment Information</h2>
+                  <p>Bank Name</p>
+                  <select
+                     className='inputbox'
+                     name='bank_name'
+                     id='bank_name'
+                     required
+                  >
+                     <option value=''>--Select Bank--</option>
+                     <option value='Bank_Papua'>Bank Papua</option>
+                     <option value='BNI'>BNI</option>
+                     <option value='Mandiri'>Mandiri</option>
+                     <option value='BCA'>BCA</option>
+                     <option value='BSI'>BSI</option>
+                     <option value='BRI'>BRI</option>
+                  </select>
+                  <p>Cardholder Name</p>
+                  <input
+                     type='text'
+                     className='inputbox'
+                     name='name'
+                     required
+                  />
+                  <p>Card Number</p>
+                  <input
+                     type='number'
+                     className='inputbox'
+                     name='card_number'
+                     id='card_number'
+                     required
+                  />
+                  <p></p>
+                  <button type='submit' className='button'>
+                     <NavLink to='/orders'>Checkout</NavLink>
+                  </button>
+               </form>
+            </div>
+            <img
+               className='product'
+               src={require("../../Assets/Images/qris.png")}
+               alt='QRIS'
             />
-            <p></p>
-            <button type="submit" className="button">
-              Checkout
-            </button>
-          </form>
-        </div>
-        <img
-          className="product"
-          src={require("../../Assets/Images/qris.png")}
-          alt="QRIS"
-        />
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Payment;
